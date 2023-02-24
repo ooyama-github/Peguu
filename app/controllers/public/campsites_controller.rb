356dbc74c3e4edf4ddda1797495232f2aa9e8b13
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-24 13:16:20 +0000

    キャンプサイトのコントローラー、更新、ビュー作成

--- app/controllers/public/campsites_controller.rb
+++ app/controllers/public/campsites_controller.rb
@@ -1,2 +1,47 @@
 class Public::CampsitesController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index]
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def new
+  end
+
+
+  def create
+  end
+
+
+  def index
+  end
+
+
+  def show
+  end
+
+
+  def edit
+  end
+
+
+  def update
+  end
+
+
+  def destroy
+  end
+
+
+  private
+
+  def campsite_params
+    params.require(:campsite).permit(:title, :opinion, :campsite_name, :facility, :campsite_image, :tag_id)
+  end
+
+  def ensure_correct_user
+    @campsite = Campsite.find(params[:id])
+    unless @campsite.user == current_user
+      redirect_to campsites_path
+    end
+  end
+
 end
