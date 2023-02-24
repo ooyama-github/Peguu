0577e2349a2a6f4cb50ed2665ece6e00ce63b194
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-24 08:53:40 +0000

    usersコントローラー編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,33 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path, alert: "～退会が完了しました、ご利用ありがとうございました～"
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
