9f80e7f07ed30c32e39e334040940590b52009c6
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-24 15:26:42 +0000

    キャンプサイトのコントローラー編集、ビュー追加

--- app/controllers/public/campsites_controller.rb
+++ app/controllers/public/campsites_controller.rb
@@ -4,30 +4,59 @@ class Public::CampsitesController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
 
   def new
+    @campsite = Campsite.new
+    @tags = Tag.all
   end
 
 
   def create
+    @campsite = Campsite.new(campsite_params)
+    @campsite.user_id = current_user.id
+    if @campsite.save
+      flash[:notice] = "投稿されました！"
+      redirect_to campsite_path(@campsite.id)
+    else
+      render :new
+    end
   end
 
 
   def index
+    @campsites = Campsite.page(params[:page]).per(18).reverse_order
+    if params[:tag_id]
+      @tag = Tag.find(params[:tag_id])
+      @campsites = @campsites.where(tag_id: params[:tag_id])
+    end
   end
 
 
   def show
+    @campsite = Campsite.find(params[:id])
+    @campsite_comment = CampsiteComment.new
   end
 
 
   def edit
+    @campsite = Campsite.find(params[:id])
   end
 
 
   def update
+    @campsite = Campsite.find(params[:id])
+    if @campsite.update(campsite_params)
+      flash[:notice] = "内容を編集しました！"
+      redirect_to campsite_path(@campsite.id)
+    else
+      render :edit
+    end
   end
 
 
   def destroy
+    @campsite = Campsite.find(params[:id])
+    @campsite.destroy
+    flash[:notice] = "削除しました！"
+    redirect_to campsites_path
   end
 
 
