f6106f8d7d8e3afba430efa1168819f5e2352e85
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-24 10:56:09 +0000

    アイテムコントローラー編集、ビューを新規作成

--- app/controllers/public/post_items_controller.rb
+++ app/controllers/public/post_items_controller.rb
@@ -1,2 +1,55 @@
 class Public::PostItemsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def new
+
+  end
+
+
+  def create
+
+  end
+
+
+  def index
+
+  end
+
+
+  def show
+
+  end
+
+
+  def edit
+
+  end
+
+
+  def update
+
+  end
+
+
+  def destroy
+
+  end
+
+
+  private
+
+  def post_item_params
+    params.require(:post_item).permit(:body, :post_dog_image)
+  end
+
+
+  def ensure_correct_user
+    @post_item = PostItem.find(params[:id])
+    unless @post_item.user == current_user
+      redirect_to post_items_path
+    end
+  end
+
 end
