2eaf2ac4147c61f4438e97b883e58454f50ae64d
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-24 13:05:44 +0000

    アイテム関連作成

--- app/controllers/public/post_items_controller.rb
+++ app/controllers/public/post_items_controller.rb
@@ -4,44 +4,61 @@ class Public::PostItemsController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
 
   def new
-
+    @post_item = PostItem.new
   end
 
 
   def create
-
+    @post_item = PostItem.new(post_item_params)
+    @post_item.user_id = current_user.id
+    if @post_item.save
+      flash[:notice] = "投稿されました！"
+      redirect_to post_item_path(@post_item.id)
+    else
+      render :new
+    end
   end
-
+  
 
   def index
-
+    @post_items = PostItem.page(params[:page]).per(18).reverse_order
   end
 
 
   def show
-
+    @post_item = PostItem.find(params[:id])
+    @post_comment = PostComment.new
   end
 
 
   def edit
-
+    @post_item = PostItem.find(params[:id])
   end
 
 
   def update
-
+    @post_item = PostItem.find(params[:id])
+    if @post_item.update(post_item_params)
+      flash[:notice] = "編集されました！"
+      redirect_to post_item_path(@post_item.id)
+    else
+      render :edit
+    end
   end
 
 
   def destroy
-
+    @post_item = PostItem.find(params[:id])
+    @post_item.destroy
+    flash[:alert] = "削除しました！"
+    redirect_to post_items_path
   end
 
 
   private
 
   def post_item_params
-    params.require(:post_item).permit(:body, :post_dog_image)
+    params.require(:post_item).permit(:body, :post_item_image)
   end
 
 
