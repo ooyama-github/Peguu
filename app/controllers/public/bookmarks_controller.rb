872c9052dd94b40df772e74444e644e6bdd41bec
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-25 08:27:59 +0000

    いいね、ブックマーク追加

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,2 +1,15 @@
 class Public::BookmarksController < ApplicationController
+  
+  def create
+    @campsite = Campsite.find(params[:campsite_id])
+    bookmark = current_user.bookmarks.new(campsite_id: @campsite.id)
+    bookmark.save
+  end 
+  
+  
+  def destroy
+    @campsite = Campsite.find(params[:campsite_id])
+    bookmark = current_user.bookmarks.find_by(campsite_id: @campsite.id)
+    bookmark.destroy
+  end
 end
--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,2 +1,16 @@
 class Public::FavoritesController < ApplicationController
+  
+  def create
+    @post_item = PostItem.find(params[:post_item_id])
+    favorite = current_user.favorites.new(post_item_id: @post_item.id)
+    favorite.save
+  end
+  
+  
+  def destroy
+    @post_item = PostItem.find(params[:post_item_id])
+    favorite = current_user.favorites.find_by(post_item_id: @post_item.id)
+    favorite.destroy
+  end 
+  
 end
