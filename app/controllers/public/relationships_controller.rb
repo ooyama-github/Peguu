3d58183167b228a3f1b38c015ca63cb22e89795d
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-25 08:54:30 +0000

    フォロー機能追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,2 +1,31 @@
 class Public::RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  
+  
+  # 非同期通信化
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+  end 
+  
+  
+  # 非同期通信化
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+  end 
+  
+  
+  #フォロー
+  def followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings.where(is_deleted: false)
+  end 
+  
+  
+  #フォロワー
+  def followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers.where(is_deleted: false)
+  end 
 end
