b923fd00f8141e86284fa3994e3669700655aeec
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-25 10:31:00 +0000

    アイテムのコメント機能追加

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,3 +1,26 @@
 class Public::PostCommentsController < ApplicationController
-  
+
+  def create
+    @post_item = PostItem.find(params[:post_item_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.post_item_id = @post_item.id
+    @post_comment.user_id = current_user.id
+    @post_comment.save
+    @post_comment = PostComment.new
+  end
+
+
+  def destroy
+    @post_item = PostItem.find(params[:post_item_id])
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    @post_comment = PostComment.new
+  end
+
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:item_comment)
+  end
 end
