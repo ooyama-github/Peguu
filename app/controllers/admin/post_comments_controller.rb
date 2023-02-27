1eb9ddba7b8541cb2b84fdecec4421ad78981193
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-27 04:05:17 +0000

    admin側のアイテム関連コントローラー追加

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,2 +1,10 @@
 class Admin::PostCommentsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def destroy
+    @post_item = PostItem.find(params[:post_item_id])
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+  end
+  
 end
--- app/controllers/admin/post_items_controller.rb
+++ app/controllers/admin/post_items_controller.rb
@@ -1,2 +1,18 @@
 class Admin::PostItemsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @post_items = PostItem.page(params[:page]).per(18).reverse_order
+  end
+
+  def show
+    @post_item = PostItem.find(params[:id])
+  end
+  
+  def destroy
+    @post_item = PostItem.find(params[:id])
+    @post_item.destroy
+    flash[:alert] = "削除しました！"
+    redirect_to admin_post_items_path
+  end
 end
