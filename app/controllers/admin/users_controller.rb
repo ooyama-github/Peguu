e496ff09e02a26975206f59ca5c8bcc72e01e883
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-27 04:07:16 +0000

    admin側ユーザーコントローラー記述追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,27 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @users = User.page(params[:page]).per(25)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:alert] = "ユーザー情報を編集しました"
+      redirect_to admin_users_path
+    else
+      render :edit
+    end 
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction, :is_deleted)
+  end 
 end
